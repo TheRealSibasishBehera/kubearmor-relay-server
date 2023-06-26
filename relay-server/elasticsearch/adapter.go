0f326df72d9b5fdf126d15c269184cbf0e274a39
Sibasish Behera <fangedhamster3114@gmail.com>
2023-06-27 04:47:06 +0530

    fix: typo and image name
    Signed-off-by: Sibasish Behera <fangedhamster3114@gmail.com>

--- relay-server/elasticsearch/adapter.go
+++ relay-server/elasticsearch/adapter.go
@@ -1,4 +1,4 @@
-package elastisearch
+package elasticsearch
 
 import (
 	"bytes"
@@ -146,6 +146,7 @@ func (ecl *ElasticsearchClient) Start() error {
 				case alert := <-ecl.alertCh:
 					ecl.bulkIndex(alert, "alert")
 				case <-ecl.ctx.Done():
+					close(ecl.alertCh)
 					return
 				}
 			}
@@ -163,7 +164,7 @@ func (ecl *ElasticsearchClient) Stop() error {
 
 	//Destoy KubeArmor Relay Client
 	if err := logClient.DestroyClient(); err != nil {
-		return fmt.Errorf("failed to destroy the kubearmor relay gRPC client (%s)\n", err.Error())
+		return fmt.Errorf("failed to destroy the kubearmor relay gRPC client (%s)", err.Error())
 	}
 	kg.Printf("Destroyed kubearmor relay gRPC client")
 
@@ -181,7 +182,7 @@ func (ecl *ElasticsearchClient) Stop() error {
 }
 
 // PrintBulkStats prints data on the bulk indexing process, including the number of indexed documents,
-// the number of errors, and the indexing rate , after elastisearch client stops
+// the number of errors, and the indexing rate , after elasticsearch client stops
 func (ecl *ElasticsearchClient) PrintBulkStats() {
 	biStats := ecl.bulkIndexer.Stats()
 	println(strings.Repeat("▔", 80))
